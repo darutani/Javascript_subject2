25015337e7701c333718b8e11cb09468a3d1d075
natsuki <su811sh1@gmail.com>
2022-10-13 10:39:58 +0900

    演算子の連続入力エラー解消後の再提出

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ let display = document.getElementById("display");
 
 function buttonPressed(Event) {
   const text = Event.target.textContent;
+  let lastWord = display.textContent.slice(-1);
 
   if (text === "=") {
-      display.textContent = eval(display.textContent);
-  } else if (text === "AC"){
-      display.textContent = 0;
+    display.textContent = eval(display.textContent);
+  } else if (text === "AC") {
+        display.textContent = 0;
+  } else if (text === "+" || text === "-" || text === "*" || text === "/") {
+        if (lastWord === "+" || lastWord === "-" || lastWord === "*" || lastWord === "/") {
+            display.textContent = display.textContent.slice(0, -1);
+            display.textContent += text;
+        } else {
+            display.textContent += text;
+        }
   } else {
-      if (display.textContent === "0") {
-        display.textContent = text;
-      } else {
-        display.textContent += text;
-      }
+        if (display.textContent === "0") {
+            display.textContent = text;
+        }
+        else {
+            display.textContent += text;
+        }
   }
 }
 
